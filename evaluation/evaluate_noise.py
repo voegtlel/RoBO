baa81a16a0ad3e216da72b458eba09f17578df25
voegtlel <voegtlel@tf.uni-freiburg.de>
2015-11-02 15:17:55 +0100

    Added evaluation: Branin with noise, solving with MCMC

---
+++ evaluation/evaluate_noise.py
@@ -0,0 +1,131 @@
+'''
+Created on 28.10.2015
+
+@author: Lukas Voegtle
+'''
+import plot_helper
+import setup_logger
+import pickle
+import cma
+import george
+from robo.maximizers.direct import Direct
+from robo.models.gaussian_process_mcmc import GaussianProcessMCMC
+from robo.priors import default_priors
+from robo.priors.base_prior import BasePrior
+from robo.solver.bayesian_optimization import BayesianOptimization
+from robo.task.branin import Branin
+from robo.task.noise_task import NoiseTask
+
+import GPy
+import matplotlib.pyplot as plt
+import numpy as np
+
+from robo.models.gpy_model import GPyModel
+from robo.acquisition.ei import EI
+from robo.maximizers.grid_search import GridSearch
+from robo.recommendation.incumbent import compute_incumbent
+from robo.task.base_task import BaseTask
+from robo.visualization.plotting import plot_objective_function, plot_model,\
+    plot_acquisition_function
+
+
+# The optimization function that we want to optimize.
+# It gets a numpy array with shape (N,D) where N >= 1 are the number of
+# datapoints and D are the number of features
+
+tasks = [Branin(), NoiseTask(Branin(), 0.1), NoiseTask(Branin(), 5)]
+
+
+class MyPrior(BasePrior):
+
+    def __init__(self, n_dims):
+        super(MyPrior, self).__init__()
+        # The number of hyperparameters
+        self.n_dims = n_dims
+        # Prior for the Matern52 lengthscales
+        self.tophat = default_priors.TophatPrior(-2, 2)
+        # Prior for the covariance amplitude
+        self.ln_prior = default_priors.LognormalPrior(mean=0.0, sigma=1.0)
+        # Prior for the noise
+        self.horseshoe = default_priors.HorseshoePrior(scale=0.1)
+
+    def lnprob(self, theta):
+        lp = 0
+        # Covariance amplitude
+        lp += self.ln_prior.lnprob(theta[0])
+        # Lengthscales
+        lp += self.tophat.lnprob(theta[1:-1])
+        # Noise
+        lp += self.horseshoe.lnprob(theta[-1])
+
+        return lp
+
+    def sample_from_prior(self, n_samples):
+        p0 = np.zeros([n_samples, self.n_dims])
+        # Covariance amplitude
+        p0[:, 0] = self.ln_prior.sample_from_prior(n_samples)
+        # Lengthscales
+        ls_sample = np.array([self.tophat.sample_from_prior(n_samples)
+                              for _ in range(1, (self.n_dims - 1))]).T
+        p0[:, 1:(self.n_dims - 1)] = ls_sample
+        # Noise
+        p0[:, -1] = self.horseshoe.sample_from_prior(n_samples)
+
+        return p0
+
+
+def global_optimize_posterior(model, X_lower, X_upper, startpoint):
+    def f(x):
+        mu, var = model.predict(x[np.newaxis, :])
+        return (mu + np.sqrt(var))[0, 0]
+    # Use CMAES to optimize the posterior mean + std
+    res = cma.fmin(f, startpoint, 0.6, options={"bounds": [X_lower, X_upper]})
+    return res[0], np.array([res[1]])
+
+
+burnin = 100
+chain_length = 200
+n_hypers = 20
+
+
+# Create figure with subplots
+
+for task_idx, task in enumerate(tasks):
+
+    cov_amp = 1.0
+    config_kernel = george.kernels.Matern52Kernel(np.ones([task.n_dims]) * 0.5,
+                                                   ndim=task.n_dims)
+
+    noise_kernel = george.kernels.WhiteKernel(0.01, ndim=task.n_dims)
+    kernel = cov_amp * (config_kernel + noise_kernel)
+
+    prior = MyPrior(len(kernel))
+
+    model = GaussianProcessMCMC(kernel, prior=prior, burnin=burnin,
+                                chain_length=chain_length, n_hypers=n_hypers)
+
+    acquisition_func = EI(model, X_upper=task.X_upper, X_lower=task.X_lower,
+                          compute_incumbent=compute_incumbent, par=0.1)
+
+    maximizer = Direct(acquisition_func, task.X_lower, task.X_upper)
+
+    bo = BayesianOptimization(acquisition_func=acquisition_func,
+                              model=model,
+                              maximize_func=maximizer,
+                              task=task,
+                              recommendation_strategy=global_optimize_posterior)
+
+    bo.run(20)
+
+    fig = plt.figure(figsize=(15, 10))
+    plt.hold(True)
+    ax_real = plt.subplot2grid((2, 2), (0, 0))
+    ax_predicted = plt.subplot2grid((2, 2), (0, 1))
+    ax_perf = plt.subplot2grid((2, 2), (1, 0), colspan=2)
+
+    plot_helper.plot_2d_contour(ax_real, ax_predicted, ax_perf, task, bo)
+
+    plt.tight_layout()
+    plt.savefig('evaluate_noise_%i.svg' % task_idx)
+
+#plt.show(block=True)
---
+++ evaluation/plot_helper.py
@@ -0,0 +1,95 @@
+"""
+Contextual Bayesian optimization, example
+"""
+
+import matplotlib.colors
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
+
+def plot_2d_contour(ax_real, ax_predicted, ax_perf, task, bo):
+    if task.do_scaling:
+        bounds_x = (task.original_X_lower[0], task.original_X_upper[0])
+        bounds_y = (task.original_X_lower[1], task.original_X_upper[1])
+    else:
+        bounds_x = (task.X_lower[0], task.X_upper[0])
+        bounds_y = (task.X_lower[1], task.X_upper[1])
+
+    # Create grid for contours
+    x = np.linspace(bounds_x[0], bounds_x[1], num=200)
+    y = np.linspace(bounds_y[0], bounds_y[1], num=200)
+    X, Y = np.meshgrid(x, y)
+    X_flat = X.flatten(order="C")
+    Y_flat = Y.flatten(order="C")
+    Z_flat = task.objective_function(np.transpose(np.array((X_flat, Y_flat)))).flatten(order="C")
+    Z = np.reshape(Z_flat, X.shape, order="C")
+
+    # Create real contour plot
+    CS = ax_real.contour(X, Y, Z, levels=[1e-3, 3e-3, 1e-2, 3e-2, 1e-1, 3e-1, 1e0, 3e0, 1e1, 3e1, 1e2, 3e2, 1e3, 3e3, 1e4],
+                     norm=matplotlib.colors.LogNorm())
+    ax_real.clabel(CS, inline=1, fontsize=10)
+    ax_real.set_title('function (payoff)')
+    ax_real.set_xlabel('x_1')
+    ax_real.set_ylabel('x_2')
+    ax_real.set_xlim(bounds_x)
+    ax_real.set_ylim(bounds_y)
+    imx, imy = np.mgrid[bounds_x[0]:bounds_x[1]:100j, bounds_y[0]:bounds_y[1]:100j]
+    resampled = griddata((X_flat, Y_flat), Z_flat, (imx, imy))
+    implt = ax_real.imshow(resampled.T, extent=(bounds_x[0], bounds_x[1], bounds_y[0], bounds_y[1]),
+                           interpolation='bicubic', origin='lower', cmap=plt.get_cmap('hot'),
+                           norm=matplotlib.colors.LogNorm(), aspect='auto')
+    resampled_limits = (resampled.min(), resampled.max())
+    plt.colorbar(implt, ax=ax_real)
+
+    # Create predicted contour plot (use the real bounds here)
+    x = np.linspace(task.X_lower[0], task.X_upper[0], num=50)
+    y = np.linspace(task.X_lower[1], task.X_upper[1], num=50)
+    X, Y = np.meshgrid(x, y)
+    X_flat = X.flatten(order="C")
+    Y_flat = Y.flatten(order="C")
+    mean, var = np.zeros(len(X_flat)), np.zeros(len(X_flat))
+    for i in range(len(X_flat)):
+        mean[i], var[i] = bo.model.predict(np.array((X_flat[i], Y_flat[i]), ndmin=2))
+    Z_flat = mean
+    Z = np.reshape(Z_flat, X.shape, order="C")
+
+    if task.do_scaling:
+        # Use plotting bounds
+        x = np.linspace(bounds_x[0], bounds_x[1], num=50)
+        y = np.linspace(bounds_y[0], bounds_y[1], num=50)
+        X, Y = np.meshgrid(x, y)
+        X_flat = X.flatten(order="C")
+        Y_flat = Y.flatten(order="C")
+
+    CS = ax_predicted.contour(X, Y, np.maximum(Z, 1e-3), levels=[1e-3, 3e-3, 1e-2, 3e-2, 1e-1, 3e-1, 1e0, 3e0, 1e1, 3e1, 1e2, 3e2,
+                                                        1e3, 3e3, 1e4], norm=matplotlib.colors.LogNorm())
+    ax_predicted.clabel(CS, inline=1, fontsize=10)
+    ax_predicted.set_title('predicted function (payoff)')
+    ax_predicted.set_xlabel('x_1')
+    ax_predicted.set_ylabel('x_2')
+    ax_predicted.set_xlim(bounds_x)
+    ax_predicted.set_ylim(bounds_y)
+    imx, imy = np.mgrid[bounds_x[0]:bounds_x[1]:100j, bounds_y[0]:bounds_y[1]:100j]
+    resampled = griddata((X_flat, Y_flat), Z_flat, (imx, imy))
+    implt = ax_predicted.imshow(resampled.T,
+                                extent=(bounds_x[0], bounds_x[1], bounds_y[0], bounds_y[1]),
+                                interpolation='bicubic', origin='lower', cmap=plt.get_cmap('hot'),
+                                norm=matplotlib.colors.LogNorm(), aspect='auto',
+                                vmin=resampled_limits[0], vmax=resampled_limits[1])
+    plt.colorbar(implt, ax=ax_predicted)
+    if task.do_scaling:
+        ax_predicted.plot(bo.X[:, 0] * (bounds_x[1] - bounds_x[0]) + bounds_x[0],
+                          bo.X[:, 1] * (bounds_y[1] - bounds_y[0]) + bounds_y[0], 'x')
+    else:
+        ax_predicted.plot(bo.X[:, 0], bo.X[:, 1], 'x')
+
+    perf = np.minimum.accumulate(bo.Y)
+    ax_perf.set_title("performance")
+    ax_perf.set_xlabel("iteration")
+    ax_perf.set_ylabel("performance")
+    ax_perf.set_yscale('log')
+    ax_perf.plot(np.arange(1, len(perf) + 1), perf.flatten(), "o")
+    ax_perf.set_xlim((0, len(perf) + 1))
+
+    if task.fopt is not None:
+        plt.axhline(task.fopt, axes=ax_perf)
